f9bfd1b404afdf2c459352f5a1b1f6392bef79b0
Pablo Jara <pablo@Pablos-MacBook-Pro.local>
2019-04-25 01:05:02 -0300

    image

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
 import React, {Component} from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import Search from './components/Search/search';
 import Results from './components/Results/results';
 import Breadcrumbs from './components/Breadcrums/breadcrumbs';
 import Home from './components/Home/home';
 import Detail from './components/Detail/detailView';
 
+
 class Index extends Component {
+  
+
   render() {
-    return (
-      <div className="App">
-        <section className="header">
-          <Search />
-        </section>
-        <Breadcrumbs></Breadcrumbs>
-        <Home></Home>
-        <section className="footer" />
-      </div>
-    );
+    return (<div className="App">
+      <section className="header">
+        <Search/>
+      </section>
+      <Breadcrumbs></Breadcrumbs>
+      <Home></Home>
+      <section className="footer"/>
+    </div>);
   }
 }
 
-function Topic({ match }) {
-  return (<>
-    <Breadcrumbs></Breadcrumbs>
-    <Detail prodId={match.params.id}></Detail>
-  < />);
+function Topic({match}) {
+  return (<section className="container content">
+
+    <div className="row">
+      <div className="col">
+        <Breadcrumbs></Breadcrumbs>
+        <Detail prodId={`https://api.mercadolibre.com/items/` + match.params.id}></Detail>
+      </div>
+    </div>
+  </section>);
 }
 
 function AppRouter() {
-  return (
-    <Router>
+  return (<Router>
     <div className="App">
-        <section className="header">
-          <Search />
-        </section>
-        <Route path="/" exact component={Home} />
-        <Route path="/details/:id" component={Topic} />
-      </div>
-    </Router>
-  );
+      <section className="header">
+        <Search/>
+      </section>
+      <Route path="/" exact="exact" component={Home}/>
+      <Route path="/details/:id" component={Topic}/>
+    </div>
+  </Router>);
 }
 
 export default AppRouter;
--- src/components/Detail/detailView.js
+++ src/components/Detail/detailView.js
@@ -1,7 +1,24 @@
 import React, {Component} from "react";
 import Breadcrumbs from '../Breadcrums/breadcrumbs';
+import axios from "axios";
 
-class DetailView extends Component{
+class DetailView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    axios.get(this.props.prodId).then(res => {
+      const data = res.data;
+      this.setState({data:data});
+      console.log(this.state.data);
+      console.log(this.state.data.pictures[0].url);
+      console.log(res.data);
+    });
+
+  }
   render() {
     let style = {
       container: "container",
@@ -9,12 +26,26 @@ class DetailView extends Component{
       col: "col",
       item: "item"
     };
-    return (<div className={style.container}>
-
-      <div className={style.row}>
-        <div className={style.col}>
-          <h1>{this.props.prodId}</h1>
-
+    return (<div className="row">
+      <div className="col-10 offset-1 p-0 bgWhite">
+        <div className={style.row}>
+          <div className="col-8">
+            <img src={this.state.data.thumbnail} alt=""/>
+          </div>
+          <div className="col-4">
+            <p className="selled">{this.state.data.condition}
+              - {this.state.data.sold_quantity}
+              vendidos</p>
+            <p className="title">{this.state.data.title}</p>
+            <p className="price">${this.state.data.price}</p>
+            <button className="buyBtn"></button>
+          </div>
+        </div>
+        <div className={style.row}>
+          <div className={style.col}>
+            <p className="descriptionTitle">Descripción del producto</p>
+            <p className="descriptionText">{this.state.data.description}</p>
+          </div>
         </div>
       </div>
     </div>);
